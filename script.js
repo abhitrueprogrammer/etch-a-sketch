363751852d3869f6db0074b0d8f8ad4b083ec4e9
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-05-27 15:38:51 +0530

    Added a button to change canvas resolution

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const canvas = document.querySelector("#canvas")
-for(let i = 0; i < 16; i++){
-    // const row = document.createElement("div");
-    // row.classList.toggle("row");
-    // canvas.appendChild(row);
-    for(let j = 0; j < 16; j++){
-        const pixel = document.createElement("div");
-        pixel.classList.toggle("pixel");
-        pixel.addEventListener("mouseenter", ()=>{
-            pixel.classList.add("red");
-        });
-        canvas.appendChild(pixel);
+createCanvas(16,16);
 
+const buttonChangeCanvas = document.querySelector("#change-canvas-size")
+
+buttonChangeCanvas.addEventListener("click", () =>{
+    let userInput = window.prompt("Enter canvas size(<100): ", "16");
+    userInput = +userInput //add sanitization and error handling
+    if(isNaN(userInput)){
+        window.alert("Please enter a single integer");
+        return
+    }
+    if(userInput > 100){
+        window.alert("Cannot enter value > 100");
+        return;
+    }
+    clearCanvas();
+    createCanvas(userInput,userInput);
+})
+function clearCanvas(){
+    canvas.textContent=""
+}
+function createCanvas(x,y) {
+    const columnPercentage = 100/x;
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y; j++) {
+            const pixel = document.createElement("div");
+            pixel.classList.toggle("pixel");
+            pixel.style.height= `${columnPercentage}%`;
+            pixel.style.flex = `1 0 ${columnPercentage}%`;
+            pixel.addEventListener("mouseenter", () => {
+                pixel.classList.add("red");
+            });
+            canvas.appendChild(pixel);
+        }
     }
 }
-
 
